938132e4e5efc98b4ec8d5fee05aec532baad784
czthxy <15705100820@163.com>
2018-11-09 10:45:23 +0800

    修改

--- app/api/project.py
+++ app/api/project.py
@@ -15,6 +15,17 @@ def project():
     return jsonify({'status': True, 'data': [to_dict(m, append_columns=['machine_names']) for m in models]})
 
 
+@api.route('/project/get')
+def project_get():
+
+    model = Project.query.get(request.args['id'])
+
+    project = {'name': model.name, "repository": model.repository, 'directory': model.directory
+        , 'save_directory': model.save_directory, 'pre_deploy': model.pre_deploy
+        , 'post_release': model.post_release, 'machine_ids': model.machine_ids}
+    return jsonify({"status": True, "data": project})
+
+
 @api.route('/project/create', methods=["POST"])
 def project_create():
     project = Project()
@@ -32,6 +43,26 @@ def project_create():
     db.session.commit()
 
     return jsonify({'status': True})
+
+
+
+@api.route('/project/update', methods=["POST"])
+def project_update():
+    project = Project.query.get(request.form['id'])
+
+    project.name = request.form['name']
+    project.repository = request.form['repository']
+    project.directory = request.form['directory']
+    project.save_directory = request.form['save_directory']
+    project.pre_deploy = request.form['pre_deploy']
+    project.post_release = request.form['post_release']
+    project.machine_ids = request.form['machine_ids']
+    if project.machine_ids == "":
+        project.machine_ids = "[]"
+
+    db.session.commit()
+
+    return jsonify({'status': True, 'data': ''})
 
 
 @api.route('/project/delete')
